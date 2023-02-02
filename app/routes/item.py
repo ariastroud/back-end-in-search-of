084fb4bb121229d0eac542948b9acbcd68c38747
Aria Stroud <stroudaria@gmail.com>
2023-02-01 19:31:40 -0800

    Adds filters

--- app/routes/item.py
+++ app/routes/item.py
@@ -19,8 +19,32 @@ def validate_model(cls, model_id):
 
 @bp.route("", methods=["GET"])
 def read_all_items():
+    filter_query = request.args.get("filter")
 
-    items = Item.query.all()
+    if filter_query == "xs":
+        items = Item.query.filter(Item.size == "XS (0-2)")
+    elif filter_query == "s":
+        items = Item.query.filter(Item.size == "S (4-6)")
+    elif filter_query == "m":
+        items = Item.query.filter(Item.size == "M (8-10)")
+    elif filter_query == "l":
+        items = Item.query.filter(Item.size == "L (12-14)")
+    elif filter_query == "xl":
+        items = Item.query.filter(Item.size == "XL (16)")
+    elif filter_query == "2xl":
+        items = Item.query.filter(Item.size == "2XL (18-20)")
+    elif filter_query == "clothing":
+        items = Item.query.filter(Item.category == "Clothing")
+    elif filter_query == "handbags":
+        items = Item.query.filter(Item.category == "Handbags")
+    elif filter_query == "jewelry":
+        items = Item.query.filter(Item.category == "Jewelry")
+    elif filter_query == "accessories":
+        items = Item.query.filter(Item.category == "Accessories")
+    elif filter_query == "shoes":
+        items = Item.query.filter(Item.category == "Shoes")
+    else:
+        items = Item.query.all()
 
     items_response = [item.to_dict() for item in items]
 
